76a33ef12865c778c7decaa15415e668cc59965e
mouse810 <mousegamingv2.0@gmail.com>
2022-08-19 15:57:41 +0530

    123

---
+++ src/pages/Login_page/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className='row'>
+                    <div className='col'>
+                        <div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default index
